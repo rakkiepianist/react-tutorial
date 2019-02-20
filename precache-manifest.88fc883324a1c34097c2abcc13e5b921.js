08d894a5e9ea3ac4285e6dbc0fcea9b5b8298d9e
Van Linh <rakkiepianist@gmail.com>
2019-02-20 11:01:26 +0700

    Updates

---
+++ precache-manifest.88fc883324a1c34097c2abcc13e5b921.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b2d15347dbf2dd6be24",
+    "url": "/react-tutorial/static/js/runtime~main.9b2d1534.js"
+  },
+  {
+    "revision": "a1cf4f087d92c08b4ff1",
+    "url": "/react-tutorial/static/js/main.a1cf4f08.chunk.js"
+  },
+  {
+    "revision": "1ad83593fd3178ca2fa5",
+    "url": "/react-tutorial/static/js/2.1ad83593.chunk.js"
+  },
+  {
+    "revision": "a1cf4f087d92c08b4ff1",
+    "url": "/react-tutorial/static/css/main.26c44d89.chunk.css"
+  },
+  {
+    "revision": "e2516567b3a4922d48d47a821ecb2d0a",
+    "url": "/react-tutorial/index.html"
+  }
+];
